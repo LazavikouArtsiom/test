1b7cf781e65483ce7d679290f80f99c19cce84c7
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-04-26 23:08:38 +0300

    extended with nested router
    did't tested

--- test/courses/api/viewsets.py
+++ test/courses/api/viewsets.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 from courses.models import Course, Lesson
-from .serializers import CourseSerializer
+from .serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
+
+class LessonViewSet(viewsets.ModelViewSet):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
