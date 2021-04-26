e1fc36b54e85a91849a23c36b6a1eb2717c6b5a9
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-04-26 14:07:43 +0300

    added urls for courses

---
+++ test/courses/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .viewsets import CourseViewSet
+from .views import LessonListCreateAPIView, LessonRetrieveUpdateDestroyAPIView
+
+router = routers.SimpleRouter()
+router.register(r'courses', CourseViewSet)
+
+urlpatterns = [
+    path('courses/<int:pk>/lessons/', LessonListCreateAPIView.as_view()),
+    path('courses/<int:pk>/lessons/<int:lesson_pk>/',
+         LessonRetrieveUpdateDestroyAPIView.as_view()),
+] + router.urls
+
+print(router.urls)
