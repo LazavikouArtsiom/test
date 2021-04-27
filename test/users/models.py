3819bf62912d0715780e8de7e49b9912d133636f
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-04-27 21:59:52 +0300

    added users app with models and default serializer

---
+++ test/users/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser,
+    PermissionsMixin
+)
+from courses.models import Course
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    nickname = models.CharField(
+        verbose_name='first name', max_length=30, blank=True, unique=True)
+    email = models.EmailField(
+        verbose_name='email address', blank=True, unique=True)
+    is_staff = models.BooleanField(
+        verbose_name='staff status',
+        default=False, )
+    is_active = models.BooleanField(
+        verbose_name='active',
+        default=True, )
+    date_joined = models.DateTimeField(
+        verbose_name='date joined', default=timezone.now)
+    
+    courses = models.ManyToManyField(Course,)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname']
+
+    def __str__(self):
+        return self.email
