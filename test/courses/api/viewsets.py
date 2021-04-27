1d6ced4c61ab2a5e0286152a46e05ab1a8e9820f
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-04-27 22:00:31 +0300

    added /subscribe /unsubscribe urls does't tested

--- test/courses/api/viewsets.py
+++ test/courses/api/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+
 from courses.models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 
@@ -6,6 +8,22 @@ from .serializers import CourseSerializer, LessonSerializer
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+    @action(detail=True, methods=['post'], permission_classes=[], name='subscribe')
+    def subscribe(self, request, pk=None):
+        user = self.request.user
+        course = Course.objects.get(pk=pk)
+        user.courses.add(course)
+        user.save()
+        return Responce({'status': 'subscription added'})
+
+    @action(detail=True, methods=['post'], permission_classes=[], name='unsubscribe')
+    def unsubscribe(self, request, pk=None):
+        user = self.request.user
+        course = Course.objects.get(pk=pk)
+        user.courses.remove(course)
+        user.save()
+        return Responce({'status': 'subscription deleted'})
 
 
 class LessonViewSet(viewsets.ModelViewSet):
