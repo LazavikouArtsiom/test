2e8904f68a11a95a8ec5598a81c315b92708e04c
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-04-29 12:32:15 +0300

    added jwt auth

--- test/config/settings.py
+++ test/config/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'homeworks',
     'quizes',
     'users',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -96,6 +97,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
