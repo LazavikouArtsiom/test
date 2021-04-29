d53b59f02fbfef68a1b206c91355883cbf00c5b3
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-04-29 12:32:27 +0300

    changed patterns structure

--- test/config/urls.py
+++ test/config/urls.py
@@ -3,9 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+api_patterns = [
     path('', include('quizes.api.urls')),
     path('', include('courses.api.urls')),
     path('', include('homeworks.api.urls')),
+    path('', include('authentication.api.urls')),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(api_patterns)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
